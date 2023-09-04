dd5cf2c851b916cac0db5a0ca0eec85ad7be76f7
rnvitas <rnvitas697@gmail.com>
2023-09-04 16:56:46 +0700

    udah ada layotnya

--- application/views/_parts/footer.php
+++ application/views/_parts/footer.php
@@ -1,24 +1,35 @@
 <footer>
-    <!-- <div class="wrapper-body"> -->
-    <div class="container-fluid">
-        <div class="col-lg-12">
-            <div class="d-flex justify-space-between">
-                <div class="col-lg-3">
-                    <div class="image">
-                        <img src="<?php echo base_url('assets/image/logo.png') ?>" class="img-fluid" width="80%">
+    <div class="wrapper-body">
+        <div class="container-fluid">
+            <div class="col-lg-12 ">
+                <div class="d-flex justify-content-between">
+                    <div class="col-lg-3 mt-5">
+                        <div class="image ">
+                            <img src="<?php echo base_url('assets/image/logo.png') ?>" class="img-fluid" width="80%">
+                        </div>
+                    </div>
+
+
+                    <div class="col-lg-7 mt-5 d-flex justify-content-end">
+                        <ul class="d-flex nav-footer">
+                            <li class="nav-link-footer"><a>Information</a></li>
+                            <li class="nav-link-footer"><a>Event</a></li>
+                            <li class="nav-link-footer"><a>Forum</a></li>
+                            <li class="nav-link-footer"><a>Repository</a></li>
+                            <li class="nav-link-footer"><a>Gallery</a></li>
+                            <li class="nav-link-footer"><a>Donation</a></li>
+                        </ul>
+
                     </div>
                 </div>
 
-                <div class="col-lg-9"></div>
-            </div>
-        </div>
-
-        <div class="col-lg-12 copyright " style="background:#e9e9e9; height: 42px;">
-            <div class="d-flex justify-content-center   ">
-                <h6 style="color:#9A8743;" class="my-2 align-items-center">Copyright © 2023 Ikatan Alumni SMAU CTARSA. All rights reserved. Design by Apa Hayo</h6>
 
             </div>
+            <div class="col-lg-12 copyright d-flex justify-content-center" style="background:#e9e9e9; height: 50px;">
+                <div class="d-flex align-items-center">
+                    <h6 style="color:#9A8743;" class="text-center">Copyright © 2023 Ikatan Alumni SMAU CTARSA. All rights reserved. Design by Apa Hayo</h6>
+
+                </div>
+            </div>
         </div>
-        <!-- </div> -->
-    </div>
 </footer>
