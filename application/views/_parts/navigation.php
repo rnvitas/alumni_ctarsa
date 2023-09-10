20e229120a487ba9f1d8fc8274de324d6ccc452f
rnvitas <rnvitas697@gmail.com>
2023-09-10 21:34:34 +0700

    responsive forum dan navigation fix

--- application/views/_parts/navigation.php
+++ application/views/_parts/navigation.php
@@ -1,61 +1,64 @@
 <div class="navigation">
-    <div class="wrapper-body">
-        <div class="container-fluid">
-            <nav class="navbar navbar-expand-lg ">
-                <div class="container">
-                    <a class="navbar-brand" href="#"><img src="<?php echo base_url('assets/image/logo.png') ?>" class="img-fluid" width="70%"></a>
+    <!-- <div class="wrapper-body">
+        <div class="container-fluid"> -->
+    <nav class="navbar navbar-expand-lg ">
+        <div class="container">
+            <a class="navbar-brand" href="#"><img src="<?php echo base_url('assets/image/logo.png') ?>" class="img-fluid" width="70%"></a>
 
 
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
 
 
-                    <div class="collapse navbar-collapse " id="navbarSupportedContent">
-                        <ul class="navbar-nav me-auto ms-auto mb-2 mb-lg-0 ">
-                            <li class="nav-item">
-                                <a class="nav-link " aria-current="page" href="#">Information</a>
+            <div class="collapse navbar-collapse " id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto ms-auto mb-2 mb-lg-0 ">
+                    <li class="nav-item">
+                        <a class="nav-link " aria-current="page" href="#">Information</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Event</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Forum</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Repository</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Gallery
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <li><a class="dropdown-item" href="#">Action</a></li>
+                            <li><a class="dropdown-item" href="#">Another action</a></li>
+                            <li>
+                                <hr class="dropdown-divider">
                             </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Event</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Forum</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Repository</a>
-                            </li>
-                            <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                    Gallery
-                                </a>
-                                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                    <li><a class="dropdown-item" href="#">Action</a></li>
-                                    <li><a class="dropdown-item" href="#">Another action</a></li>
-                                    <li>
-                                        <hr class="dropdown-divider">
-                                    </li>
-                                    <li><a class="dropdown-item" href="#">Something else here</a></li>
-                                </ul>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link " href="#" tabindex="-1" aria-disabled="true">Donation</a>
-                            </li>
+                            <li><a class="dropdown-item" href="#">Something else here</a></li>
                         </ul>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Blogs</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link " href="#" tabindex="-1" aria-disabled="true">Donation</a>
+                    </li>
+                </ul>
 
-                        <div class="d-flex align-items-center">
-                            <!-- <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"> -->
-                            <button class="btn btn-outline-primary me-3" type="submit">Contact Us</button>
+                <div class="d-flex align-items-center">
+                    <!-- <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"> -->
+                    <button class="btn btn-outline-primary me-3" type="submit">Contact Us</button>
 
-                            <button class="btn btn-primary" type="submit">Sign in</button>
-                        </div>
-                    </div>
-
-
+                    <button class="btn btn-primary" type="submit">Sign in</button>
                 </div>
-            </nav>
+            </div>
+
+
         </div>
-    </div>
+    </nav>
+    <!-- </div>
+    </div> -->
 </div>
 
 <body style="background:#f5f5f5;">
