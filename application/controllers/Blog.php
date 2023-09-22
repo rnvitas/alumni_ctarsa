862ebadb85b378a62182d3c3767faae62abe6ffc
rnvitas <rnvitas697@gmail.com>
2023-09-22 18:28:52 +0700

    base blog

---
+++ application/controllers/Blog.php
@@ -0,0 +1,83 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Blog extends CI_Controller
+{
+
+
+    function __construct()
+    {
+        parent::__construct();
+
+        $this->load->model('Blog_m', 'blog');
+    }
+    public function index()
+    {
+
+        $data['category'] = $this->info->getAllType();
+        $data['kabaralumni'] = $this->info->getKabarAlumni();
+        $data['populer'] = $this->info->getPopuler();
+        $data['terbaru'] = $this->info->getTerbaru();
+
+
+
+        $data['content']     = 'webview/blog/information_view';
+        $data['content_js'] = 'webview/information/information_js';
+        $this->load->view('_parts/wrapper', $data);
+    }
+
+    public function detail($id = '')
+    {
+        $id = $_GET['id'];
+
+        $data['populer'] = $this->info->getPopuler();
+        $data['category'] = $this->info->getAllType();
+        $data['detail_information'] = $this->info->getInformation_by_id($id);
+        $data['content']     = 'webview/information/detail_information_view';
+        $data['content_js'] = 'webview/information/information_js';
+        $this->load->view('_parts/wrapper', $data);
+        $this->add_count($id);
+    }
+
+    function add_count($id)
+    {
+        // $id = $_GET['id'];
+        // load cookie helper
+        $this->load->helper('cookie');
+        // this line will return the cookie which has slug name
+        $check_visitor = $this->input->cookie(urldecode($id), FALSE);
+        // this line will return the visitor ip address
+        $ip = $this->input->ip_address();
+        // if the visitor visit this article for first time then //
+        // //set new cookie and update article_views column ..
+        // //you might be notice we used slug for cookie name and ip
+        // //address for value to distinguish between articles views
+        if ($check_visitor == false) {
+            $cookie = array("name" => urldecode($id), "value" => "$ip", "expire" => time() + 7200, "secure" => false);
+            $this->input->set_cookie($cookie);
+            $this->info->update_counter(urldecode($id));
+        }
+    }
+
+    function populer_section()
+    {
+
+        $data['all_populer'] = $this->info->getAllPopuler();
+        $data['populer'] = $this->info->getPopuler();
+        $data['category'] = $this->info->getAllType();
+        $data['content']     = 'webview/information/popular_view';
+        $data['content_js'] = 'webview/information/information_js';
+        $this->load->view('_parts/wrapper', $data);
+    }
+
+
+    function newest_section()
+    {
+        $data['all_terbaru'] = $this->info->getAllTerbaru();
+        $data['terbaru'] = $this->info->getTerbaru();
+        $data['category'] = $this->info->getAllType();
+        $data['content']     = 'webview/information/terbaru_view';
+        $data['content_js'] = 'webview/information/information_js';
+        $this->load->view('_parts/wrapper', $data);
+    }
+}
---
+++ application/views/webview/blog/detail_view.php
@@ -0,0 +1,180 @@
+ <div class="wrapper-body">
+     <div class=" col-lg-12">
+         <p class="headerblog">kategori</p>
+         <h1 class="headerblog1">Judul Blog yang Keren Lorem ipsum dolor sit amet consectetur adipiscing</h1>
+         <p class="headerblog2">Minggu, 29 Agustus 2023 | Adib Dwi Kusuma</p>
+     </div>
+ </div>
+
+
+ <section id="blogdetails" class="blogdetails">
+     <div class="wrapper-body">
+         <div class="container">
+             <div class="judul col-lg-12">
+                 <h3>H2 Untuk Section Lorem Ipsum Dolor Sit</h3>
+                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu dolor felis. Nunc egestas
+                     orci in lobortis elementum. Donec sem nisl, tincidunt vel erat a, ultricies tristique erat.
+                     Cras efficitur vestibulum libero in varius. Cras tellus sem, pellentesque non eros et,
+                     porttitor lacinia justo. Quisque ac dolor vitae ante sodales tristique eget a tortor. Donec
+                     sed ex facilisis, vulputate arcu et, efficitur massa.</p>
+             </div>
+             <img class="img-fluid mt-5" src="assets/img/image 56 (1).png" alt="">
+             <p class="d-flex justify-content-center mt-2" style="color: rgba(0, 0, 0, 0.50);">Photo by Lorem Ipsum on Dolor Sit Amet</p>
+
+             <div class="article1" style="margin-top: 10%;">
+                 <h3>Vestibulum tincidunt libero orci</h3>
+                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu dolor felis. Nunc egestas orci in lobortis elementum. Donec sem nisl, tincidunt vel erat a, ultricies tristique erat. Cras efficitur vestibulum libero in varius. Cras tellus sem, pellentesque non eros et, porttitor lacinia justo. Quisque ac dolor vitae ante sodales tristique eget a tortor. Donec sed ex facilisis, vulputate arcu et, efficitur massa.</p>
+             </div>
+
+             <div class="article2" style="margin-top: 10%;">
+                 <h3>Vestibulum tincidunt libero orci</h3>
+                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu dolor felis. Nunc egestas orci in lobortis elementum. Donec sem nisl, tincidunt vel erat a, ultricies tristique erat. Cras efficitur vestibulum libero in varius. Cras tellus sem, pellentesque non eros et, porttitor lacinia justo. Quisque ac dolor vitae ante sodales tristique eget a tortor. Donec sed ex facilisis, vulputate arcu et, efficitur massa.</p>
+             </div>
+
+             <img class="img-fluid" src="assets/img/image 63.png" alt="" style="margin-top: 10%;">
+             <p class="d-flex justify-content-center mt-2" style="color: rgba(0, 0, 0, 0.50);">Photo by Lorem Ipsum on Dolor Sit Amet</p>
+         </div>
+     </div>
+
+     <div class="wrapper-body">
+         <p class="readmorep">Baca juga dari kategori lomba</p>
+         <div class="bacajuga">
+             <div class="card single_post">
+                 <div class="body">
+                     <div class="row">
+                         <div class="col-md-10">
+                             <div class="col-lg-12 d-flex justify-content-between">
+                                 <div class="col-lg-6">
+                                     <h3>
+                                         <a href="blog-details.html">All photographs are accurate</a>
+                                     </h3>
+                                 </div>
+                                 <div class="col-lg-6 d-flex justify-content-end">
+                                     <p>17 Desember 2023</p>
+                                 </div>
+
+                             </div>
+                             <p>
+                                 It is a long established fact that a reader will be distracted
+                                 by the readable content of a page when looking at its layout.
+                                 The point of using Lorem Ipsum is that it has a more-or-less
+                                 normal It is a long established fact that a reader will be distracted
+                                 by the readable content of a page when looking at its layout.
+                                 The point of using Lorem Ipsum is that it has a more-or-less
+                                 normal
+                             </p>
+                         </div>
+                         <div class="col-md-2 d-flex justify-content-between">
+                             <div class="img-post">
+                                 <img class="d-block img-fluid" src="assets/img/blog/image 56.png" alt="First slide" />
+                             </div>
+                         </div>
+                     </div>
+
+                 </div>
+             </div>
+             <div class="card single_post">
+                 <div class="body">
+                     <div class="row">
+                         <div class="col-md-10">
+                             <div class="col-lg-12 d-flex justify-content-between">
+                                 <div class="col-lg-6">
+                                     <h3>
+                                         <a href="blog-details.html">All photographs are accurate</a>
+                                     </h3>
+                                 </div>
+                                 <div class="col-lg-6 d-flex justify-content-end">
+                                     <p>17 Desember 2023</p>
+                                 </div>
+
+                             </div>
+                             <p>
+                                 It is a long established fact that a reader will be distracted
+                                 by the readable content of a page when looking at its layout.
+                                 The point of using Lorem Ipsum is that it has a more-or-less
+                                 normal It is a long established fact that a reader will be distracted
+                                 by the readable content of a page when looking at its layout.
+                                 The point of using Lorem Ipsum is that it has a more-or-less
+                                 normal
+                             </p>
+                         </div>
+                         <div class="col-md-2 d-flex justify-content-between">
+                             <div class="img-post">
+                                 <img class="d-block img-fluid" src="assets/img/blog/image 56.png" alt="First slide" />
+                             </div>
+                         </div>
+                     </div>
+
+                 </div>
+             </div>
+             <div class="card single_post">
+                 <div class="body">
+                     <div class="row">
+                         <div class="col-md-10">
+                             <div class="col-lg-12 d-flex justify-content-between">
+                                 <div class="col-lg-6">
+                                     <h3>
+                                         <a href="blog-details.html">All photographs are accurate</a>
+                                     </h3>
+                                 </div>
+                                 <div class="col-lg-6 d-flex justify-content-end">
+                                     <p>17 Desember 2023</p>
+                                 </div>
+
+                             </div>
+                             <p>
+                                 It is a long established fact that a reader will be distracted
+                                 by the readable content of a page when looking at its layout.
+                                 The point of using Lorem Ipsum is that it has a more-or-less
+                                 normal It is a long established fact that a reader will be distracted
+                                 by the readable content of a page when looking at its layout.
+                                 The point of using Lorem Ipsum is that it has a more-or-less
+                                 normal
+                             </p>
+                         </div>
+                         <div class="col-md-2 d-flex justify-content-between">
+                             <div class="img-post">
+                                 <img class="d-block img-fluid" src="assets/img/blog/image 56.png" alt="First slide" />
+                             </div>
+                         </div>
+                     </div>
+
+                 </div>
+             </div>
+             <div class="card single_post cards">
+                 <div class="body">
+                     <div class="row">
+                         <div class="col-md-10">
+                             <div class="col-lg-12 d-flex justify-content-between">
+                                 <div class="col-lg-6">
+                                     <h3>
+                                         <a href="blog-details.html">All photographs are accurate</a>
+                                     </h3>
+                                 </div>
+                                 <div class="col-lg-6 d-flex justify-content-end">
+                                     <p>17 Desember 2023</p>
+                                 </div>
+
+                             </div>
+                             <p>
+                                 It is a long established fact that a reader will be distracted
+                                 by the readable content of a page when looking at its layout.
+                                 The point of using Lorem Ipsum is that it has a more-or-less
+                                 normal It is a long established fact that a reader will be distracted
+                                 by the readable content of a page when looking at its layout.
+                                 The point of using Lorem Ipsum is that it has a more-or-less
+                                 normal
+                             </p>
+                         </div>
+                         <div class="col-md-2 d-flex justify-content-between">
+                             <div class="img-post">
+                                 <img class="d-block img-fluid" src="assets/img/blog/image 56.png" alt="First slide" />
+                             </div>
+                         </div>
+                     </div>
+
+                 </div>
+             </div>
+         </div>
+     </div>
+ </section>
